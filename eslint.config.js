112c39e40da1b222f7c2337ad621da78c2e196eb
Khalil Ltaief <ltaief.khalil@gmail.com>
2026-06-03 14:06:46 +0200

    refactor: improve formatting and readability in eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,19 @@
-import config from "@christopherjbaker/eslint-config/react-strict";
-import { defineConfig, globalIgnores } from "eslint/config";
-import globals from "globals";
+import config from "@christopherjbaker/eslint-config/react-strict"
+import { defineConfig, globalIgnores } from "eslint/config"
+import globals from "globals"
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-export default defineConfig(globalIgnores(["dist/", "web-build/"]), config, {
-  // configs overrides, if need
-}, {
-  // CommonJS config files (babel.config.cjs, jest.config.cjs) need Node globals.
-  files: ["*.cjs"],
-  languageOptions: {
-    globals: globals.node,
+export default defineConfig(
+  globalIgnores(["dist/", "web-build/"]),
+  config,
+  {
+    // configs overrides, if need
   },
-});
+  {
+    // CommonJS config files (babel.config.cjs, jest.config.cjs) need Node globals.
+    files: ["*.cjs"],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+)
